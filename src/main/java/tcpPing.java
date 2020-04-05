b7d39744f525533b696e76b1cf2881332f6e5367
Volkan Isik <s180103@student.dtu.dk>
2020-04-05 19:54:34 +0200

    almost done

--- src/main/java/tcpPing.java
+++ src/main/java/tcpPing.java
@@ -12,26 +12,42 @@ public class tcpPing {
 
         System.out.println("Enter url/ip number to ping ?");
         String pingTo = scan.nextLine();
-        try {
-            server = new Socket(pingTo, 80);
-            fromServer = new BufferedReader((new InputStreamReader(server.getInputStream())));
-            toServer = new DataOutputStream(server.getOutputStream());
+        System.out.println("Set a timeout in milliseconds: ");
+        int timeout = scan.nextInt();
+        System.out.println("Set number of requests to send: ");
+        int request = scan.nextInt();
 
-            System.out.println(server.isConnected());
+        for (int i = 0; i < request; i++) {
+
+            try {
+                server = new Socket(pingTo, 80);
+                fromServer = new BufferedReader((new InputStreamReader(server.getInputStream())));
+                toServer = new DataOutputStream(server.getOutputStream());
+                server.setSoTimeout(timeout);
 
 
-            toServer.writeBytes("HEAD / HTTP/1.1 " + CRLF);
-            toServer.writeBytes("Host: " + pingTo + CRLF +CRLF);
-            long time1 = System.currentTimeMillis();
+               // System.out.println(server.isConnected());
+                toServer.writeBytes("HEAD / HTTP/1.1 " + CRLF);
+                toServer.writeBytes("Host: " + pingTo + CRLF + CRLF);
+
+                long time1 = System.currentTimeMillis();
 
 
+                String response=" ";
+                response = fromServer.readLine();
+                long time2 = System.currentTimeMillis();
+                if(!response.equals(" "))
+                System.out.println("Responce time: " + (time2 - time1) + " msec from: " + server.getInetAddress());
 
-            System.out.println(fromServer.readLine());
-            long time2 = System.currentTimeMillis();
-            System.out.println("Responce time: "+(time2-time1)+"msec from: "+server.getInetAddress() );
-
-            server.close();
-        }catch (IOException e) {
-            e.printStackTrace();}
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
